f7a6ee4475eb29adc93d46efc1c3c4419435dca3
Popescu Alexandru <56479261+alexpopescu021@users.noreply.github.com>
2020-04-08 22:07:09 +0300

    added Search

--- InternshipProject/wwwroot/js/site.js
+++ InternshipProject/wwwroot/js/site.js
@@ -3,3 +3,43 @@
 
 // Write your JavaScript code.
 //cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js
+
+function filterTable(event) {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#exportTable tbody").rows;
+
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[1].textContent.toUpperCase();
+        var secondCol = rows[i].cells[2].textContent.toUpperCase();
+        var thirdCol = rows[i].cells[3].textContent.toUpperCase();
+        var fourthCol = rows[i].cells[4].textContent.toUpperCase();
+        var fifthCol = rows[i].cells[5].textContent.toUpperCase();
+
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || thirdCol.indexOf(filter) > -1 || fourthCol.indexOf(filter) > -1 || fifthCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
+
+document.querySelector('#myInput').addEventListener('keyup', filterTable, false);
+
+$(function () {
+    $("#exporttable").click(function (e) {
+        var table = $("#exportTable");
+        if (table && table.length) {
+            $(table).table2excel({
+                exclude: ".noExl",
+                name: "Excel Document Name",
+                filename: "BBBootstrap" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
+                fileext: ".xls",
+                exclude_img: true,
+                exclude_links: true,
+                exclude_inputs: true,
+                preserveColors: false
+            });
+        }
+    });
+
+});
