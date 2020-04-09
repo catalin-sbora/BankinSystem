a362646710bfa8479f302ff031530c53dace9b72
Popescu Alexandru <56479261+alexpopescu021@users.noreply.github.com>
2020-04-09 14:53:36 +0300

    Update site.js

--- InternshipProject/wwwroot/js/site.js
+++ InternshipProject/wwwroot/js/site.js
@@ -3,8 +3,11 @@
 
 // Write your JavaScript code.
 //cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js
-
+//<link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+//    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+ //   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 function filterTable(event) {
+
     var filter = event.target.value.toUpperCase();
     var rows = document.querySelector("#exportTable tbody").rows;
 
