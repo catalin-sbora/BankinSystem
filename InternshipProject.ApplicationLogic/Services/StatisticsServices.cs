df0b19afb177cfda1fa02d0a03c551ac80c00b5a
catalin-sbora <catalin.sbora@gmail.com>
2020-04-09 08:45:32 -0700

    update for partial view

--- InternshipProject.ApplicationLogic/Services/StatisticsServices.cs
+++ InternshipProject.ApplicationLogic/Services/StatisticsServices.cs
@@ -4,6 +4,7 @@ using InternshipProject.ApplicationLogic.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace InternshipProject.ApplicationLogic.Services
@@ -16,7 +17,24 @@ namespace InternshipProject.ApplicationLogic.Services
         {
             this.customerRepository = customerRepository;
         }
-       
+
+        private IEnumerable<decimal> ProcessBalanceHistory(decimal initialBalance, IEnumerable<Transaction> transactions)
+        {
+            List<decimal> balanceOverTime = new List<decimal>();
+            var currentBalance = initialBalance;
+            foreach (Transaction transaction in transactions)
+            {
+                currentBalance -= transaction.Amount;
+                balanceOverTime.Add(currentBalance);
+            }
+            return balanceOverTime.AsEnumerable();
+        }
+
+        private IEnumerable<Transaction> FilterAccountTransactions(BankAccount account, Func<Transaction, bool> condition)
+        {
+            return account.Transactions.Where(condition)
+                                       .OrderByDescending(t => t.Time);
+        }
 
         public IEnumerable<BankAccount> GetCustomerBankAccounts(string userId)
         {
@@ -34,34 +52,44 @@ namespace InternshipProject.ApplicationLogic.Services
 
         public IEnumerable<decimal> BankAccountHistoryAllTime(BankAccount bankAccount)
         {
-            List<decimal> balanceOverTime = new List<decimal>();
-            var currentBalance = bankAccount.Balance;
-            var sortedTransactions = bankAccount.Transactions.OrderByDescending(param => param.Time);
-
-            foreach(Transaction transaction in sortedTransactions)
-            {
-                currentBalance -= transaction.Amount;
-                balanceOverTime.Add(currentBalance);
-            }
-
-            return balanceOverTime.AsEnumerable();
+            
+            var sortedTransactions = FilterAccountTransactions(bankAccount, t => true);
+            return ProcessBalanceHistory(bankAccount.Balance, sortedTransactions);
         }
 
         public IEnumerable<decimal> BankAccountHistoryYear(BankAccount bankAccount)
         {
-            List<decimal> balanceOverTime = new List<decimal>();
-            var currentBalance = bankAccount.Balance;
-            var sortedTransactions = bankAccount.Transactions.Where(transaction => 
-                                                                transaction.Time.Year >= DateTime.UtcNow.Year - 1)
-                                                                .OrderByDescending(param => param.Time);
 
-            foreach (Transaction transaction in sortedTransactions)
-            {
-                currentBalance -= transaction.Amount;
-                balanceOverTime.Add(currentBalance);
-            }
+            var sortedTransactions = FilterAccountTransactions(bankAccount,
+                                                                transaction =>
+                                                                DateTime.UtcNow
+                                                                        .Subtract(transaction.Time)
+                                                                        .Days <= 365);
 
-            return balanceOverTime.AsEnumerable();
+            return ProcessBalanceHistory(bankAccount.Balance, sortedTransactions);
+        }
+
+        public IEnumerable<decimal> BankAccountHistoryMonth(BankAccount bankAccount)
+        {
+
+            var sortedTransactions = bankAccount.Transactions.Where(transaction =>
+                                                                transaction.Time.Year == DateTime.UtcNow.Year
+                                                                &&
+                                                                transaction.Time.Month == DateTime.UtcNow.Month);
+
+            return ProcessBalanceHistory(bankAccount.Balance, sortedTransactions);
+        }
+
+        public IEnumerable<decimal> BankAccountHistoryWeek(BankAccount bankAccount)
+        {
+
+            var sortedTransactions = FilterAccountTransactions(bankAccount,
+                                                                transaction =>
+                                                                DateTime.UtcNow
+                                                                        .Subtract(transaction.Time)
+                                                                        .Days <= 7);
+
+            return ProcessBalanceHistory(bankAccount.Balance, sortedTransactions);
         }
     }
 }
